9fd2f817b82b7072781d63d2d9124b48a4b61a40
xandy-716 <151407509+xandy-716@users.noreply.github.com>
2025-03-17 12:23:35 -0700

    added sql models

--- FlaskProject/models/judge.py
+++ FlaskProject/models/judge.py
@@ -1,9 +1,12 @@
 # models/judge.py
-from app import db  # Assuming 'db' is defined in app.py
 
-class Judge(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(128), nullable=False)
-    # Add other fields and methods as needed
+from app import db 
+
+class judge(db.Model):
+    judge_id = db.Column(db.Integer, primary_key=True)
+    judge_name = db.Column(db.String(50), nullable=False)
+    projects_assigned = db.Column(db.Integer)
+    perc_complete = db.Column(db.Integer)
+    credential_id = db.Column(db.Integer)
+
+# add more functionality as needed
